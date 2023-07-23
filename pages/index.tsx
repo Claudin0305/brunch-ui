e6201b0388a1d9cfc50b8837a960f025c9dbb2a4
Claudin Saintil <claudinsaintil@gmail.com>
2023-07-23 15:39:34 -0400

    test for error double 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,8 @@ const Home: React.FC<Props> = ({ data }) => {
 export async function getServerSideProps() {
   try {
     // Fetch data from an API or perform other async operations
-    const response = await axios.get(`${process.env.base_route}/events`);
+    // const response = await axios.get(`${process.env.base_route}/events`);
+    const response = await axios.get(`http://localhost:8080/api/events`);
     const data = response.data;
 
     // Return the data as props
