5f0f6729bf7f0c49ccdca5a7ca507e0395a21aad
Claudin Saintil <claudinsaintil@gmail.com>
2023-08-22 15:10:51 -0400

    refactoring for dev

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    base_route: "http://isteah-tech.ddns.net:1094/api/api",
+    // base_route: "http://isteah-tech.ddns.net:1094/api/api",
     // base_route: "http://grahn-brunch.ddns.net:9094/api/api",
-    // base_route: "http://localhost:8080/api",
-    // base_route_get: "http://localhost:8080/api",
-    base_route_get: "http://localhost:8080/api/api",
+    base_route: "http://localhost:8080/api",
+    base_route_get: "http://localhost:8080/api",
+    // base_route_get: "http://localhost:8080/api/api",
   },
   images: {
     domains: ['localhost', "grahn-brunch.ddns.net", "isteah-tech.ddns.net"],
