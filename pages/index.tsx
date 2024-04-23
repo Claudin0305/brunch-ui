9786898e5344591c0f40d7a2f7c71bb53e1eada7
Claudin Saintil <claudinsaintil@gmail.com>
2024-04-23 15:46:19 -0400

    added sec

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,7 @@ const Home: React.FC<Props> = ({ data }) => {
     <>
       <Head>
         <title>Evenements</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="description" content="Brunch" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
