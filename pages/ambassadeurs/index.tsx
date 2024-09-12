079aeeecded251062b34349512210a4a1f3bf184
Claudin Saintil <claudinsaintil@gmail.com>
2024-09-12 13:03:47 -0400

    refactoring

--- pages/ambassadeurs/index.tsx
+++ pages/ambassadeurs/index.tsx
@@ -7,7 +7,8 @@ const Ambassadeur = () => {
       <Head>
         <title>Tableau des ambassadeurs</title>
       </Head>
-    <div className="px-8 md:px-32 mx-auto md:mt-32 mt-16 mb-32">En construction</div>
+    <div className="px-8 md:px-32 mx-auto md:mt-32 mt-16 mb-32">
+        <h1 className="mt-8 text-xl md:text-2xl text-center">En construction</h1></div>
       </HomeLayout>
   )
 }
