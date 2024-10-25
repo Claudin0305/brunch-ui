acf3eb454700b6719805587d7eb744378134f458
Claudin Saintil <claudinsaintil@gmail.com>
2024-10-25 17:14:14 -0400

    refactoring

--- components/core/barometer.tsx
+++ components/core/barometer.tsx
@@ -175,6 +175,28 @@ const Barometer: React.FC = () => {
           </div>
         </div>
       </div>} */}
+      {data &&
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            {/* <XAxis dataKey="name" /> */}
+            {/* <YAxis /> */}
+            {/* <Tooltip /> */}
+            <Legend />
+            <Bar dataKey="promesse" stackId="a" fill="#1e40af" />
+            <Bar dataKey="don" stackId="a" fill="#dc2626" />
+          </BarChart>
+        </ResponsiveContainer>}
     </>
   );
 };
