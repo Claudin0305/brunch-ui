60f21cb1d6e834bc2e2a067025b60e6c8695f0fa
Claudin Saintil <claudinsaintil@gmail.com>
2024-10-25 20:26:07 -0400

    refactoring

--- components/core/barometer.tsx
+++ components/core/barometer.tsx
@@ -60,23 +60,23 @@ const Barometer: React.FC = () => {
 
   return (
     <>
-      {data && <div className="flex flex-col items-center mt-12">
-        <h1 className="text-4xl font-bold text-blue-800 mb-8">Thermomètre de la Collecte de Fonds: BRUNCH 2024</h1>
+      {data && <div className="flex flex-col items-center mt-8">
+        {/* <h1 className="text-4xl font-bold text-blue-800 mb-8">Thermomètre de la Collecte de Fonds: BRUNCH 2024</h1> */}
 
         {/* Barre horizontale pour l'objectif */}
-        <div className="relative w-3/5 my-10">
-          <div className="w-full h-2 border-b-4 border-dashed border-green-500" />
-          <div className="absolute top-[-30px] left-1/2 transform -translate-x-1/2 font-bold text-red-600 text-xl">
+        <div className="relative w-3/5 my-4">
+          <div className="w-full h-2 border-b-4 border-dashed border-[#D62828]" />
+          <div className="absolute top-[-30px] left-1/2 transform -translate-x-1/2 text-gray-900 text-xl">
             Objectif : ${data?.objectif}
           </div>
         </div>
 
         {/* Thermomètres */}
-        <div className="flex justify-center items-center space-x-10">
+        <div className="flex justify-center items-center flex-col">
           {/* Thermomètre des montants promis */}
-          <div className="flex flex-col  space-x-0 space-y-10">
-            <p className='font-bold text-blue-800 text-xl'>Don promis: ${data?.promesse}</p>
-            <p className='text-red-600 text-xl font-bold'>Don reçus: ${data?.don}</p>
+          <div className="flex flex-col  space-x-0 space-y-4">
+
+            {/* <p className='text-red-600 text-xl font-bold'>Don reçus: ${data?.don}</p> */}
           </div>
           {/* <div className="relative w-24 h-96 bg-gray-200">
             <div className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 font-bold text-green-500 text-lg">
@@ -92,10 +92,10 @@ const Barometer: React.FC = () => {
           </div> */}
 
           {/* Thermomètre des montants reçus */}
-          <div className="relative w-24 h-96 bg-gray-200">
-            <div className="h-full">
-              <div className="bg-blue-800 h-1/2 w-full" style={{ height: `${pourcentagePromesses}%` }}></div>
-              <div className="bg-red-600 h-1/2 w-full" style={{ height: `${pourcentageRecus}%` }}></div>
+          <div className="relative w-24 h-72 bg-gray-200">
+            <div className="h-72 bg-gray-200 flex flex-col justify-end">
+              <div className="bg-[#1F4E79] w-full mt-auto" style={{ height: `${pourcentagePromesses}%`}}></div>
+              <div className="bg-[#D62828] w-full" style={{ height: `${pourcentageRecus}%` }}></div>
             </div>
             {/* <div className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 font-bold text-green-500 text-lg">
 
@@ -103,99 +103,11 @@ const Barometer: React.FC = () => {
 
 
           </div>
+          <p className='text-[#1F4E79] text-xl'>Don promis: ${data?.promesse}</p>
+          <p className='text-[#D62828] text-xl'>Don reçus: ${data?.don}</p>
         </div>
       </div>}
-      {/* <div className='flex flex-col'>
-      {data && <div className='md:pl-12 flex flex-col md:flex-row mb-8 space-y-8 space-x-0 md:space-x-8 md:space-y-0 tex-white'>
-        <p className='text-xl w-full md:w-1/3 bg-red-600 text-center p-4 rounded-lg text-white'><span className='font-semibold'>Objectif:</span>{data.objectif} $CA</p>
-        <p className='text-xl w-full md:w-1/3 bg-gray-200 text-center p-4 rounded-lg text-gray-900'><span className='font-semibold'>Promesses:</span>{data.promesse} $CA</p>
-        <p className='text-xl w-full md:w-1/3 bg-lime-600 text-center p-4 rounded-lg text-white'><span className='font-semibold'>Dons:</span>{data.don} $CA</p>
-      </div>
-      }
 
-
-       <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={data_}>
-
-        <Tooltip />
-        <Legend />
-          <Bar dataKey="Objectif" fill="#dc2626" barSize={100} />
-          <Bar dataKey="Dons" fill="#65a30d" barSize={100} />
-          <Bar dataKey="Promesses" fill="#d1d5db" barSize={100} />
-      </BarChart>
-    </ResponsiveContainer>
-
-    </div> */}
-      {/* {data && <div className="text-center mt-12">
-        <h1 className="text-4xl font-bold text-[#1F4E79] mb-8">
-          Thermomètre de la Collecte de Fonds: BRUNCH 2024
-        </h1>
-
-
-        <div className="w-3/5 mx-auto my-10 relative">
-          <div className="h-2 border-b-4 border-dashed border-[#4CAF50]" />
-          <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 text-[#D62828] font-bold text-xl">
-            Objectif : ${data?.objectif}
-          </div>
-        </div>
-
-        <div className="flex justify-center items-end space-x-16 w-3/5 mx-auto">
-
-          <div className="relative w-24 h-96 bg-gray-300">
-            <div className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 font-bold text-[#4CAF50]">
-              Montant promis
-            </div>
-            <div
-              className="absolute bottom-0 w-full bg-[#1F4E79] transition-all duration-500 ease-in-out"
-              style={{ height: `${pourcentagePromesses}%` }}
-            />
-            <div className="absolute bottom-[calc(100%-${pourcentagePromesses}%)] left-1/2 transform -translate-x-1/2 text-white font-bold">
-              ${data?.promesse}
-            </div>
-            <div className="absolute bottom-[-30px] left-1/2 transform -translate-x-1/2 text-[#1F4E79] font-bold">
-              ${data?.promesse}
-            </div>
-          </div>
-
-          <div className="relative w-24 h-96 bg-gray-300">
-            <div className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 font-bold text-[#4CAF50]">
-              Montant reçu
-            </div>
-            <div
-              className="absolute bottom-0 w-full bg-[#D62828] transition-all duration-500 ease-in-out"
-              style={{ height: `${pourcentageRecus}%` }}
-            />
-            <div className="absolute bottom-[calc(100%-${pourcentageRecus}%)] left-1/2 transform -translate-x-1/2 text-white font-bold">
-              ${data?.don}
-            </div>
-            <div className="absolute bottom-[-30px] left-1/2 transform -translate-x-1/2 text-[#D62828] font-bold">
-              ${data?.don}
-            </div>
-          </div>
-        </div>
-      </div>} */}
-      {data &&
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            width={500}
-            height={300}
-            data={data_}
-            margin={{
-              top: 20,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Promesses" stackId="a" fill="#1e40af" />
-            <Bar dataKey="Dons" stackId="a" fill="#dc2626" />
-          </BarChart>
-        </ResponsiveContainer>}
     </>
   );
 };
--- pages/campagne-2024/index.tsx
+++ pages/campagne-2024/index.tsx
@@ -16,14 +16,14 @@ const Campagne2024: React.FC<Props> = ({ data }) => {
             </Head>
             <div className="px-8 md:px-32 mx-auto md:mt-32 mt-16 mb-32">
                 <div className='text-center mb-8 mt-40'>
-                    <p className='font-bold text-md md:text-2xl capitalize text-center text-blue-500 mb-8'>"Campagne 2024" Je fais ma part</p>
+                    <p className='font-semibold text-md md:text-2xl capitalize text-center mb-8'>"Campagne 2024" Je fais ma part</p>
 
                     <div className="flex flex-col space-y-8 md:space-y-0 md:grid md:grid-cols-2 md:gap-8">
                         <a href={'https://www.zeffy.com/donation-form/56c2d8e2-95e7-4776-835c-7fed3405bd11 '} target="_blank" className="w-full">
-                            <Button className="bg-blue-500 capitalize w-full" variant="contained">Faire un don</Button>
+                            <Button className="bg-blue-500 capitalize w-1/2" variant="contained">Faire un don</Button>
                         </a>
                         <a href={'https://foof90mb.forms.app/jefaismapart2024 '} target="_blank" className="w-full">
-                            <Button className="bg-blue-500 capitalize w-full" variant="contained">Faire une promesse de don</Button>
+                            <Button className="bg-blue-500 capitalize w-1/2" variant="contained">Faire une promesse de don</Button>
                         </a>
                     </div>
                 </div>
